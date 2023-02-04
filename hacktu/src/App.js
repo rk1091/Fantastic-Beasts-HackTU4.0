f1a7a7e2ed970ee13a47e1487053e9403f893329
Ananya <ananyajian2209@gmail.com>
2023-02-04 20:12:51 +0530

    Basic setup 4 components added

--- hacktu/src/App.js
+++ hacktu/src/App.js
@@ -1,8 +1,27 @@
+import React from "react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+
+import Navbar from "./Components/Navbar.jsx";
+import Login from "./Components/Login.jsx";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Register from "./Components/Register.jsx";
+
+import * as ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Team Fantastic Beasts rk says hello</h1>
-    </div>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
   );
 }
 
