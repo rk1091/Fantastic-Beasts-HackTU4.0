d7bd0e19cc11064502522cc51e0ff1b720ccb76d
rk1091 <riya_kapoor@hotmail.com>
2023-02-05 07:48:41 +0530

    uff

--- hacktu/src/Components/Treatmentcard.jsx
+++ hacktu/src/Components/Treatmentcard.jsx
@@ -13,132 +13,64 @@ const IMAGE =
 
 export default function Treatmentcard() {
   return (
-    <div>
-      {" "}
-      <Center py={12}>
+    <Center py={12}>
+      <Box
+        role={"group"}
+        p={6}
+        maxW={"330px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        pos={"relative"}
+        zIndex={1}>
         <Box
-          role={"group"}
-          p={6}
-          maxW={"330px"}
-          w={"full"}
-          bg={useColorModeValue("white", "gray.800")}
-          boxShadow={"2xl"}
           rounded={"lg"}
+          mt={-12}
           pos={"relative"}
-          zIndex={1}>
-          <Box
+          height={"230px"}
+          _after={{
+            transition: "all .3s ease",
+            content: '""',
+            w: "full",
+            h: "full",
+            pos: "absolute",
+            top: 5,
+            left: 0,
+            backgroundImage: `url(${IMAGE})`,
+            filter: "blur(15px)",
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: "blur(20px)",
+            },
+          }}>
+          <Image
             rounded={"lg"}
-            mt={-12}
-            pos={"relative"}
-            height={"230px"}
-            _after={{
-              transition: "all .3s ease",
-              content: '""',
-              w: "full",
-              h: "full",
-              pos: "absolute",
-              top: 5,
-              left: 0,
-              backgroundImage: `url(${IMAGE})`,
-              filter: "blur(15px)",
-              zIndex: -1,
-            }}
-            _groupHover={{
-              _after: {
-                filter: "blur(20px)",
-              },
-            }}>
-            <Image
-              rounded={"lg"}
-              height={230}
-              width={282}
-              objectFit={"cover"}
-              src={IMAGE}
-            />
-          </Box>
-          <Stack pt={10} align={"center"}>
-            <Text
-              color={"gray.500"}
-              fontSize={"sm"}
-              textTransform={"uppercase"}>
-              Brand
-            </Text>
-            <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-              Nice Chair, pink
-            </Heading>
-            <Stack direction={"row"} align={"center"}>
-              <Text fontWeight={800} fontSize={"xl"}>
-                $57
-              </Text>
-              <Text textDecoration={"line-through"} color={"gray.600"}>
-                $199
-              </Text>
-            </Stack>
-          </Stack>
+            height={230}
+            width={282}
+            objectFit={"cover"}
+            src={IMAGE}
+          />
         </Box>
-      </Center>
-      <Center py={12}>
-        <Box
-          role={"group"}
-          p={6}
-          maxW={"330px"}
-          w={"full"}
-          bg={useColorModeValue("white", "gray.800")}
-          boxShadow={"2xl"}
-          rounded={"lg"}
-          pos={"relative"}
-          zIndex={1}>
-          <Box
-            rounded={"lg"}
-            mt={-12}
-            pos={"relative"}
-            height={"230px"}
-            _after={{
-              transition: "all .3s ease",
-              content: '""',
-              w: "full",
-              h: "full",
-              pos: "absolute",
-              top: 5,
-              left: 0,
-              backgroundImage: `url(${IMAGE})`,
-              filter: "blur(15px)",
-              zIndex: -1,
-            }}
-            _groupHover={{
-              _after: {
-                filter: "blur(20px)",
-              },
-            }}>
-            <Image
-              rounded={"lg"}
-              height={230}
-              width={282}
-              objectFit={"cover"}
-              src={IMAGE}
-            />
-          </Box>
-          <Stack pt={10} align={"center"}>
-            <Text
-              color={"gray.500"}
-              fontSize={"sm"}
-              textTransform={"uppercase"}>
-              Brand
+        <Stack pt={10} align={"center"}>
+          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+            Brand
+          </Text>
+          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+            Nice Chair, pink
+          </Heading>
+          <Stack direction={"row"} align={"center"}>
+            <Text fontWeight={800} fontSize={"xl"}>
+              $57
             </Text>
-            <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-              Nice Chair, pink
-            </Heading>
-            <Stack direction={"row"} align={"center"}>
-              <Text fontWeight={800} fontSize={"xl"}>
-                $57
-              </Text>
-              <Text textDecoration={"line-through"} color={"gray.600"}>
-                $199
-              </Text>
-            </Stack>
+            <Text textDecoration={"line-through"} color={"gray.600"}>
+              $199
+            </Text>
           </Stack>
-        </Box>
-      </Center>
-    </div>
+        </Stack>
+      </Box>
+    </Center>
   );
 }
